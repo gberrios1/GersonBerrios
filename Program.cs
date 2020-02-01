16d8c6b24440453ff0a5207f79c4b6d0b5e64d33
gberrios1 <60244459+gberrios1@users.noreply.github.com>
2020-01-31 22:37:49 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GersonBerrios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display 
+            Console.WriteLine("Hi, this is Gerson Berrios!");
+            Console.WriteLine("Let me know how you are doing - type your answer below and hit enter:");
+
+            //Setting the string variable
+            string pacho;
+            pacho = Console.ReadLine();
+
+            Console.WriteLine("You typed: {0}", pacho); 
+        }
+    }
+}
